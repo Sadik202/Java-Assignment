e062aa46de38ea7c032b15125115ab9b8be60531
Sadik Rahman <sadikbohail01@gmail.com>
2025-07-26 23:49:36 +0600

    Create Even1-100.java

---
+++ Even1-100.java
@@ -0,0 +1,14 @@
+public class SeriesOfEven {
+   public static void main(String[] args) {
+    int i;
+    
+    System.out.println("1 to 100 Even Numbers:");
+    
+    for(i=2;i<=100;i+=2)
+    {
+        System.out.print(i +" ");
+    }
+           
+   }
+
+}
